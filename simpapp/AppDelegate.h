842a5102438024abb86f9058f34ff6ae2c10c4a9
Delgado-Moore <Delgado@Saphire.local>
2014-12-21 13:26:28 -0800

    Initial Commit

---
+++ simpapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  simpapp
+//
+//  Created by Delgado-Moore on 12/21/14.
+//  Copyright (c) 2014 delgado-moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
